22fd521c5ea9d9e74129e9d4d0a373e19b2b7d0c
John Doe <john.doe@contoso.com>
2022-10-07 14:29:09 -0500

    ch7 event hub project

---
+++ ch7-2/Contracts/Detail.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge7EventHub.Contracts
+{
+    public class Detail
+    {
+        [JsonProperty("productId")]
+        public Guid ProductId { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("unitCost")]
+        public decimal UnitCost { get; set; }
+
+        [JsonProperty("totalCost")]
+        public decimal TotalCost { get; set; }
+
+        [JsonProperty("totalTax")]
+        public decimal TotalTax { get; set; }
+
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("productDescription")]
+        public string ProductDescription { get; set; }
+
+
+    }
+}
---
+++ ch7-2/SalesOrderEventHubFunction.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Challenge7EventHub
+{
+    public class SalesOrderEventHubFunction
+    {
+        [FunctionName("SalesOrderEventHubFunction")]
+        public async Task Run([EventHubTrigger("bfyoc-sales", Connection = "EventHubConnectionString")] EventData[] events,
+            [CosmosDB(
+                    databaseName:"Sales",
+                    collectionName:"Orders",
+                    ConnectionStringSetting ="IceCreamRatingsCosmosDbConnection")] IAsyncCollector<Document> orders,
+            ILogger log)
+        {
+            var exceptions = new List<Exception>();
+
+            log.LogInformation($"Invoked with {events.Length} events...");
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    
+
+                    string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+
+                    var order = Newtonsoft.Json.JsonConvert.DeserializeObject<Document>(messageBody);
+                    if (null != order)
+                    {
+                        await orders.AddAsync(order);
+                    }
+
+                    // Replace these two lines with your processing logic.
+                    //log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+    }
+}
